3ba07f5fa67f715e4edb938ceaa747391da10c2e
KalebMolewe23 <molewekaleb33@gmail.com>
2022-12-08 07:50:40 -0800

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://erestadev.com/';
-$config['base_url'] = 'http://localhost/eresta_server/';
+$config['base_url'] = 'https://erestadev.com/';
+// $config['base_url'] = 'http://localhost/eresta_server/';
 
 /*
 |--------------------------------------------------------------------------
