b2e58f3a321c16021ca76cc488961a48ce477353
KalebMolewe23 <molewekaleb33@gmail.com>
2023-02-22 03:13:26 -0800

    config

--- application/views/home/layout/footer.php
+++ application/views/home/layout/footer.php
@@ -8,10 +8,7 @@
                         <p><i class="fa fa-phone-alt me-3"></i>+62 895-3372-61274</p>
                         <p><i class="fa fa-envelope me-3"></i>erestadeveloper@gmail.com</p>
                         <div class="d-flex pt-2">
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-instagram"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                            <a class="btn btn-outline-light btn-social" href="https://www.instagram.com/eresta.dev/"><i class="fab fa-instagram"></i></a>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-3">
